0e96e81645f08f80d07387480c939f3803e55191
radzuweitraphael <72080616+radzuweitraphael@users.noreply.github.com>
2025-07-18 10:24:55 +0200

    Create __init__.py

---
+++ KPOKPCH/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.0.1"
+
+from importlib import import_module
+
+__all__ = [
+    "initialize_dmfm",
+    "_construct_state_matrices",
+    "kalman_smoother_dmfm",
+    "em_step_dmfm",
+    "fit_dmfm_em",
+]
+
+def __getattr__(name):
+  if name in __all__:
+    module = import_module(".dmfm", __name__)
+    attr = getattr(module, name)
+    globals()[name] = attr
+    return attr
+  raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
