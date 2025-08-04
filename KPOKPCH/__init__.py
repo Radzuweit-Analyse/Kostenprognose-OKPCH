be77d82fa896f62835dd6663c478750afe08f1fb
Cairoknox <72080616+Cairoknox@users.noreply.github.com>
2025-08-04 16:28:47 +0200

    Cleanup

--- KPOKPCH/__init__.py
+++ KPOKPCH/__init__.py
@@ -2,15 +2,25 @@ __version__ = "0.0.1"
 
 from importlib import import_module
 
-__all__ = [
+__dmfm__ = [
     "DMFM",
+]
+
+__utils__ = [
     "seasonal_difference",
 ]
 
+__all__ = __dmfm__ + __utils__
+
 
 def __getattr__(name):
-    if name in __all__:
+    if name in __dmfm__:
         module = import_module(".dmfm", __name__)
+        attr = getattr(module, name)
+        globals()[name] = attr
+        return attr
+    elif name in __utils__:
+        module = import_module(".utils", __name__)
         attr = getattr(module, name)
         globals()[name] = attr
         return attr
